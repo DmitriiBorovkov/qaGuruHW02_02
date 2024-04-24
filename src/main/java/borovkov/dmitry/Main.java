c79f14ea4b83b68e470bef2a52c32c27d2a297e1
Dmitrii Borovkov <167776826+DmitriiBorovkov@users.noreply.github.com>
2024-04-24 17:30:38 +0300

    Update Main.java

--- src/main/java/borovkov/dmitry/Main.java
+++ src/main/java/borovkov/dmitry/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static String msg = "Здарова, щеглы!";
     public static void main(String[] args) {
         System.out.println("Hello world! Что-то я тут поменял");
-        System.out.println("Hello qaGuru");
+        System.out.println("Hello qaGuru111");
         System.out.println(msg);
     }
 }
