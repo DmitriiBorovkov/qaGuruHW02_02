cd94ba630dc0abb73ccd8f4b9bde356127e08f1f
Dmitrii Borovkov <167776826+DmitriiBorovkov@users.noreply.github.com>
2024-04-24 17:10:57 +0300

    Update Main.java
    Еще раз поменял

--- src/main/java/borovkov/dmitry/Main.java
+++ src/main/java/borovkov/dmitry/Main.java
@@ -1,8 +1,11 @@
 package borovkov.dmitry;
 
 public class Main {
+
+    privat static String msg = "Здарова, щеглы!"
     public static void main(String[] args) {
         System.out.println("Hello world! Что-то я тут поменял");
         System.out.println("Hello qaGuru");
+        System.out.println(msg);
     }
 }
